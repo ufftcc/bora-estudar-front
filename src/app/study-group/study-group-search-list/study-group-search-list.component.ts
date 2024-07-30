3084e7e0baa28b87a246080602308a61b83b7fe9
ciro germano <ciromrs@gmail.com>
2024-07-30 00:02:57 -0300

    fix: studyGroup service response mapping

--- src/app/study-group/study-group-search-list/study-group-search-list.component.ts
+++ src/app/study-group/study-group-search-list/study-group-search-list.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { StudyGroup } from '../study-group';
-import { StudyGroupMockService } from '../study-group-mock.service';
 import { StudyGroupService } from '../study-group.service';
-import { catchError, Observable, of } from 'rxjs';
 import { StudyGroupSearchItemComponent } from '../study-group-search-item/study-group-search-item.component';
 import { NgFor } from '@angular/common';
 
 @Component({
-    selector: 'app-study-group-search-list',
-    templateUrl: './study-group-search-list.component.html',
-    styleUrls: ['./study-group-search-list.component.scss'],
-    standalone: true,
-    imports: [NgFor, StudyGroupSearchItemComponent],
+  selector: 'app-study-group-search-list',
+  templateUrl: './study-group-search-list.component.html',
+  styleUrls: ['./study-group-search-list.component.scss'],
+  standalone: true,
+  imports: [NgFor, StudyGroupSearchItemComponent],
 })
 export class StudyGroupSearchListComponent implements OnInit {
   studyGroups: StudyGroup[] = [];
 
-  constructor(
-    public service: StudyGroupService) {}
+  constructor(public service: StudyGroupService) {}
 
   ngOnInit() {
-    this.service.getStudyGroups().subscribe((dados) => {
-      console.error(dados)
-      this.studyGroups = dados;
-    })
+    this.getStudyGroups();
+  }
+
+  getStudyGroups(): void {
+    this.service
+      .getStudyGroups()
+      .subscribe((studyGroups) => (this.studyGroups = studyGroups));
   }
 }
